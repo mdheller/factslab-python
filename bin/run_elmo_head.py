ec8bb153d30c6993fb302c08f83b8f3760b835ef
venkatasg <gvenkata1994@gmail.com>
2018-10-08 14:48:06 -0400

    Added attention support and skeleton of span+sentence attention formulae is up

--- bin/run_elmo_head.py
+++ bin/run_elmo_head.py
@@ -1,20 +1,15 @@
 import argparse
 import numpy as np
 import pandas as pd
-from factslab.utility import load_glove_embedding, arrange_inputs
-from torch.nn import LSTM
-# from factslab.datastructures import ConstituencyTree
-# from factslab.datastructures import DependencyTree
-# from factslab.pytorch.childsumtreelstm import ChildSumDependencyTreeLSTM
-from factslab.pytorch.rnnregression import RNNRegressionTrainer
+from factslab.utility import ridit
+from factslab.pytorch.mlpregression import MLPTrainer
 from torch.cuda import is_available
 from torch import device
-from torch import max
 from allennlp.modules.elmo import Elmo
-import sys
+from sklearn.utils import shuffle
 
 # initialize argument parser
-description = 'Run an RNN regression on Genericity protocol annotation.'
+description = 'Run a simple MLP with(out) attention on varying levels on ELMO.'
 parser = argparse.ArgumentParser(description=description)
 
 parser.add_argument('--protocol',
@@ -28,31 +23,52 @@ parser.add_argument('--embeddings',
                     default='/Users/venkat/Downloads/embeddings/')
 parser.add_argument('--regressiontype',
                     type=str,
-                    default="multinomial")
+                    default="linear")
 parser.add_argument('--epochs',
                     type=int,
                     default=1)
 parser.add_argument('--batch',
                     type=int,
                     default=128)
-parser.add_argument('--rnntype',
-                    type=str,
-                    default="linear")
 parser.add_argument('--verbosity',
                     type=int,
                     default="1")
-parser.add_argument('--lr',
-                    type=float,
-                    default="0.01")
-parser.add_argument('--L2',
-                    type=float,
-                    default="0")
 parser.add_argument('--attention',
                     action='store_true',
                     help='Turn attention on or off')
+parser.add_argument('--span',
+                    action='store_true',
+                    help='Turn span attention on or off')
+parser.add_argument('--sentence',
+                    action='store_true',
+                    help='Turn sentence attention on or off')
+parser.add_argument('--paramattention',
+                    action='store_true',
+                    help='Turn param attention on or off')
 
 # parse arguments
 args = parser.parse_args()
+
+# Find out the attention type to be used based on arguments
+if not args.attention:
+    attention_type = "None"
+else:
+    if not args.paramattention:
+        if args.span and not args.sentence:
+            attention_type = "Span"
+        elif args.sentence and not args.span:
+            attention_type = "Sentence"
+        else:
+            # Span + sentence?
+            pass
+    else:
+        if args.span and not args.sentence:
+            attention_type = "Span-param"
+        elif args.sentence and not args.span:
+            attention_type = "Sentence-param"
+        else:
+            # Span + sentence?
+            pass
 
 if args.protocol == "noun":
     datafile = "../../../protocols/data/noun_long_data.tsv"
@@ -63,6 +79,7 @@ if args.protocol == "noun":
     attr_conf = {"part": "Part.Confidence", "kind": "Kind.Confidence",
              "abs": "Abs.Confidence"}
     token_col = "Noun.Token"
+    root_token = "Noun.Root.Token"
 else:
     datafile = "../../../protocols/data/pred_long_data.tsv"
     response = ["Is.Particular", "Is.Hypothetical", "Is.Dynamic"]
@@ -71,52 +88,45 @@ else:
     attr_map = {"part": "Is.Particular", "dyn": "Is.Dynamic", "hyp": "Is.Hypothetical"}
     attr_conf = {"part": "Part.Confidence", "dyn": "Dyn.Confidence",
              "hyp": "Hyp.Confidence"}
-    token_col = "Pred.Root.Token"
+    token_col = "Pred.Token"
+    root_token = "Pred.Root.Token"
 
 data = pd.read_csv(datafile, sep="\t")
 
-data['SentenceID.Token'] = data['Sentence.ID'].map(lambda x: x) + "_" + data[token_col].map(lambda x: str(x))
+data['Split.Sentence.ID'] = data.apply(lambda x: x['Split'] + " sent_" + str(x['Sentence.ID']), axis=1)
+
+data['Unique.ID'] = data.apply(lambda x: x['Split'] + " sent_" + str(x['Sentence.ID']) + "_" + str(x[token_col]), axis=1)
+
+# Load the structures
+structures = {}
+
+# Don't read_csv the structures file. read_csv can't handle quotes
+with open(args.structures, 'r') as f:
+    for line in f.readlines():
+        structs = line.split('\t')
+        structures[structs[0]] = structs[1].split()
+
+data['Structure'] = data['Split.Sentence.ID'].map(lambda x: structures[x])
 
 # Split the datasets into train, dev, test
 data_test = data[data['Split'] == 'test']
 data_dev = data[data['Split'] == 'dev']
 data = data[data['Split'] == 'train']
 
-# Convert responses to 1s and 0s
-for resp in response:
-    data[resp] = data[resp].astype(int)
+# Ridit scoring annotations and confidence ratings
+for attr in attributes:
+    resp = attr_map[attr]
+    resp_conf = attr_conf[attr]
+    data[resp_conf + ".norm"] = data.groupby('Annotator.ID')[resp_conf].transform(ridit)
+    data[resp + ".norm"] = data[resp].map(lambda x: 1 if x else -1) * data[resp_conf + ".norm"]
+    data_dev[resp_conf + ".norm"] = data_dev.groupby('Annotator.ID')[resp_conf].transform(ridit)
+    data_dev[resp + ".norm"] = data_dev[resp].map(lambda x: 1 if x else -1) * data_dev[resp_conf + ".norm"]
 
-if args.regressiontype == "multinomial":
-    # make smallest response value 0
-    for resp in response_conf:
-        data[resp] = data.groupby('Annotator.ID')[resp].apply(lambda x: x.rank() / (len(x) + 1.))
+# Shuffle the data
+data = shuffle(data)
+data_dev = shuffle(data_dev)
+data_test = shuffle(data_test)
 
-else:
-    # convert response confs to logit ridit scores
-    for resp in response_conf:
-        data[resp] = data.groupby('Annotator.ID')[resp].apply(lambda x: x.rank() / (len(x) + 1.))
-        data[resp] = np.log(data[resp]) - np.log(1. - data[resp])
-
-# Load the structures
-# structures = {}
-# vocab = []
-
-# # Don't read_csv the structures file. read_csv can't handle quotes
-# with open(args.structures, 'r') as f:
-#     for line in f.readlines():
-#         structs = line.split('\t')
-#         structures[structs[0]] = DependencyTree.fromstring(structs[1])
-#         structures[structs[0]].sentence = structs[2].split()
-#         vocab.append(structs[2].split())
-
-# data['Structure'] = data['Sentence.ID'].map(lambda x: structures[x])
-
-# vocab = list(set(sum(vocab, [])))
-data_dev = data[data['Split'] == 'dev']
-data = data[data['Split'] == 'train']
-
-# load the glove embedding
-# embeddings = load_glove_embedding(args.embeddings, vocab)
 # ELMO embeddings
 options_file = args.embeddings + "options.json"
 weight_file = args.embeddings + "weights.hdf5"
@@ -124,56 +134,52 @@ elmo = Elmo(options_file, weight_file, 1, dropout=0.5)
 
 # pyTorch figures out device to do computation on
 device_to_use = device("cuda:0" if is_available() else "cpu")
-# Shuffle the data
-data = data.sample(frac=1).reset_index(drop=True)
-data_dev = data_dev.sample(frac=1).reset_index(drop=True)
-data_test = data_test.sample(frac=1).reset_index(drop=True)
-# Define attributes for regression
 
+# Prepare all the inputs
 
-# Handle linear LSTM here. Minibatch can be done
-rnntype = LSTM
-x_raw = [struct.sentence for struct in data['Structure']]
-x = [x_raw[i:i + args.batch] for i in range(0, len(x_raw), args.batch)]
-tokens_raw = data[token_col].values
-tokens = [tokens_raw[i:i + args.batch] for i in range(0, len(tokens_raw), args.batch)]
-y = {}
-loss_wts = {}
-for attr in attributes:
-    y_raw = data[attr_map[attr]].values
-    y[attr] = [y_raw[i:i + args.batch] for i in range(0, len(y_raw), args.batch)]
-    y[attr][-1] = np.append(y[attr][-1], y[attr][-2][0:len(y[attr][-2]) - len(y[attr][-1])])
-    wt_raw = data[attr_conf[attr]].values
-    loss_wts[attr] = [wt_raw[i:i + args.batch] for i in range(0, len(wt_raw), args.batch)]
-    loss_wts[attr][-1] = np.append(loss_wts[attr][-1], loss_wts[attr][-2][0:len(loss_wts[attr][-2]) - len(loss_wts[attr][-1])])
+x = [data['Structure'].values.tolist()[i:i + args.batch] for i in range(0, len(data['Structure']), args.batch)]
+tokens = [data[token_col].values.tolist()[i:i + args.batch] for i in range(0, len(data[token_col]), args.batch)]
+y = []
+# loss_wts = {}
+
+y = [{attr: (data[attr_map[attr] + ".norm"].values[i:i + args.batch]) for attr in attributes} for i in range(0, len(data[attr_map[attr] + ".norm"].values), args.batch)]
+y[-1] = {attr: np.append(y[-1][attr], y[-2][attr][0:len(y[-2][attr]) - len(y[-1][attr])]) for attr in attributes}
+
+# wt_raw = data[attr_conf[attr]].values
+# loss_wts[attr] = [wt_raw[i:i + args.batch] for i in range(0, len(wt_raw), args.batch)]
+# loss_wts[attr][-1] = np.append(loss_wts[attr][-1], loss_wts[attr][-2][0:len(loss_wts[attr][-2]) - len(loss_wts[attr][-1])])
 
 x[-1] = x[-1] + x[-2][0:len(x[-2]) - len(x[-1])]    # last batch size hack
 tokens[-1] = np.append(tokens[-1], tokens[-2][0:len(tokens[-2]) - len(tokens[-1])])
 
-# Arrange in descending order and pad each batch
-x, y, loss_wts, lengths, tokens = arrange_inputs(data_batch=x,
-                                                 targets_batch=y,
-                                                 wts_batch=loss_wts,
-                                                 tokens_batch=tokens,
-                                                 attributes=attributes)
+# Create dev data
+data_dev_mean = data_dev.groupby('Unique.ID', as_index=False)[token_col, 'Is.Particular.norm', 'Is.Kind.norm', 'Is.Abstract.norm'].mean()
+data_dev_mean['Structure'] = data_dev_mean['Unique.ID'].map(lambda x: data_dev[data_dev['Unique.ID'] == x]['Structure'].iloc[0])
 
-dev_x = [struct.sentence for struct in data_dev['Structure']]
-dev_tokens = np.array(data_dev[token_col].tolist()) + 1
+dev_x = [data_dev_mean['Structure'].values.tolist()[i:i + args.batch] for i in range(0, len(data_dev_mean['Structure']), args.batch)]
+dev_tokens = [data_dev_mean[token_col].values.tolist()[i:i + args.batch] for i in range(0, len(data_dev_mean[token_col]), args.batch)]
+# loss_wts = {}
 
 dev_y = {}
-dev_wts = {}
 for attr in attributes:
-    dev_y[attr] = data_dev[attr_map[attr]].tolist()
-    dev_wts[attr] = data_dev[attr_conf[attr]].tolist()
+    dev_y[attr] = [data_dev_mean[attr_map[attr] + ".norm"].values[i:i + args.batch] for i in range(0, len(data_dev_mean[attr_map[attr] + ".norm"].values), args.batch)]
+    dev_y[attr][-1] = np.append(y[-1][attr], y[-2][attr][0:len(y[-2][attr]) - len(y[-1][attr])])
 
-# train the model
-trainer = RNNRegressionTrainer(embeddings=elmo, device=device_to_use,
-                               rnn_classes=rnntype,
-                               bidirectional=True, attention=False,
-                               regression_type=args.regressiontype,
-                               rnn_hidden_sizes=300, num_rnn_layers=1,
-                               regression_hidden_sizes=(150,),
-                               epochs=args.epochs, batch_size=args.batch,
-                               attributes=attributes)
+for attr in attributes:
+    dev_y[attr] = np.concatenate(dev_y[attr], axis=None)
+# wt_raw = data_dev_mean[attr_conf[attr]].values
+# loss_wts[attr] = [wt_raw[i:i + args.batch] for i in range(0, len(wt_raw), args.batch)]
+# loss_wts[attr][-1] = np.append(loss_wts[attr][-1], loss_wts[attr][-2][0:len(loss_wts[attr][-2]) - len(loss_wts[attr][-1])])
 
-trainer.fit(X=x, Y=y, tokens=tokens, verbosity=args.verbosity, lengths=lengths, lr=args.lr, weight_decay=args.L2, dev=[dev_x, dev_tokens, dev_y, dev_wts])
+dev_x[-1] = dev_x[-1] + dev_x[-2][0:len(dev_x[-2]) - len(dev_x[-1])]    # last batch size hack
+dev_tokens[-1] = np.append(dev_tokens[-1], dev_tokens[-2][0:len(dev_tokens[-2]) - len(dev_tokens[-1])])
+
+# Initialise the model
+trainer = MLPTrainer(embeddings=elmo, device=device_to_use,
+                     attributes=attributes, attention=attention_type,
+                     regressiontype=args.regressiontype)
+
+# Training phase
+trainer.fit(X=x, Y=y, tokens=tokens, verbosity=args.verbosity, dev=[dev_x, dev_y, dev_tokens])
+
+# Save the model
--- factslab/utility/tree.py
+++ factslab/utility/tree.py
@@ -2,6 +2,7 @@
 # from nltk import DependencyGraph
 import re
 from os.path import expanduser
+from os import remove
 
 
 def html_ify(s):
@@ -18,11 +19,16 @@ files = ['/UD_English-r1.2/en-ud-train.conllu',
          '/UD_English-r1.2/en-ud-test.conllu']
 home = expanduser("~/Downloads/")
 structures = []
+
+# Delete file if it already exists since it'll be opened in append mode
+struct_path = expanduser("~/Desktop") + "/protocols/data/structures.tsv"
+remove(struct_path)
+
 for file in files:
     path = home + file
-    with open('../../../../protocols/data/structures.tsv', 'a') as fout:
+    with open(struct_path, 'a') as fout:
         with open(path, 'r') as f:
-            id = 0
+            idNo = 0
             a = ""
             words = []
             for line in f:
@@ -30,12 +36,12 @@ for file in files:
                     a += line
                     words.append(line.split("\t")[1])
                 else:
-                    id += 1
+                    idNo += 1
                     a = html_ify(a)
                     # structure = DependencyGraph(a, top_relation_label='root')
                     sent = " ".join(words)
                     sent = html_ify(sent)
-                    sent_id = file + " sent_" + str(id)
+                    sent_id = file[23:][:-7] + " sent_" + str(idNo)
                     # structures.append(structure)
                     a = ""
                     words = []
